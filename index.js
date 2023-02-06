eb902bdc8e761ee71209c5485e5d4d4f58d7e3de
Payton <psides83@hotmail.com>
2023-02-06 07:23:53 -0600

    added circuits and rookie

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const PORT = process.env.PORT || 8000;
 import express from "express";
-import getWpraLb from "./src/wpar-lb.js";
-import getWpraBr from "./src/wpra-br.js";
+import getWpra from "./src/wpra.js";
 
 const app = express();
 
-app.get("/wpra-br/:year", async (req, res) => {
-  const year = req.params.year;
+app.get("/:event/:type/:year/:circuit?", async (req, res) => {
+  const { event, type, year, circuit } = req.params;
 
-  const brStandings = await getWpraBr(year);
+  const standings = await getWpra(event, type, year, circuit);
+  console.log(event, type, year, circuit);
 
-  res.json(brStandings);
-});
-
-app.get("/wpra-lb/:year", async (req, res) => {
-  const year = req.params.year;
-
-  const lbStandings = await getWpraLb(year);
-
-  res.json(lbStandings);
+  res.json(standings);
 });
 
 // todo PORT running
