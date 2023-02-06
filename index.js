8694f710467749d8fb31fdd625e61468bf9aa069
Payton <psides83@hotmail.com>
2023-02-06 10:25:20 -0600

    handle url error

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get("/:event/:type/:year/:circuit?", async (req, res) => {
   const { event, type, year, circuit } = req.params;
 
   const standings = await getWpra(event, type, year, circuit);
-  console.log(event, type, year, circuit);
 
   res.json(standings);
 });
--- src/wpra.js
+++ src/wpra.js
@@ -46,59 +46,69 @@ export default async function getWpra(event, type, year, circuit) {
   }
 
   console.log(event, type, year);
-  const response = await axios.get(url());
-  const html = response.data;
+  if (url()) {
+    const response = await axios.get(url());
+    const html = response.data;
 
-  // Load the HTML into Cheerio
-  const $ = cheerio.load(html);
+    // Load the HTML into Cheerio
+    const $ = cheerio.load(html);
 
-  // Find the table in the HTML
-  const table = $("table");
+    // Find the table in the HTML
+    const table = $("table");
 
-  // Find the rows in the table
-  const rows = table.find("tr");
-  console.log(table);
+    // Find the rows in the table
+    const rows = table.find("tr");
+    console.log(table);
 
-  var rawData = [];
+    var rawData = [];
 
-  // Iterate over the rows and get the values from the 1st and 9th columns
-  rows.each((i, row) => {
-    const rankRaw = $(row).find("td:nth-child(1)").text();
-    const Place = Number(rankRaw);
-    const nameRaw = $(row).find("td:nth-child(2)").text();
-    const name = nameRaw.split(" ").filter((name) => !name.includes("("));
-    const FirstName = name[0];
-    const LastName = name[name.length - 1];
-    const Hometown = $(row).find("td:nth-child(3)").text();
-    const earningsRaw = $(row).find("td:nth-child(4)").text();
-    const Earnings = parseFloat(earningsRaw.replace(/,/g, "").replace("$", ""));
-    const Event = "GB";
-    const Type = "world";
-    const Points = Earnings;
-    const SeasonYear = Number(year);
-    const StandingId = 0;
-    const ContestantId = 0;
+    // Iterate over the rows and get the values from the 1st and 9th columns
+    rows.each((i, row) => {
+      const rankRaw = $(row).find("td:nth-child(1)").text();
+      const Place = Number(rankRaw);
+      const nameRaw = $(row).find("td:nth-child(2)").text();
+      const name = nameRaw.split(" ").filter((name) => !name.includes("("));
+      const FirstName = name[0];
+      const LastName = name[name.length - 1];
+      const Hometown = $(row).find("td:nth-child(3)").text();
+      const earningsRaw = $(row).find("td:nth-child(4)").text();
+      const Earnings = parseFloat(
+        earningsRaw.replace(/,/g, "").replace("$", "")
+      );
+      const Event = "GB";
+      const Type = "world";
+      const Points = Earnings;
+      const SeasonYear = Number(year);
+      const StandingId = 0;
+      const ContestantId = 0;
 
-    if (name) {
-      rawData.push({
-        ContestantId,
-        Place,
-        FirstName,
-        LastName,
-        Hometown,
-        Earnings,
-        Event,
-        Type,
-        Points,
-        SeasonYear,
-        StandingId,
-      });
-    }
-  });
+      if (name) {
+        rawData.push({
+          ContestantId,
+          Place,
+          FirstName,
+          LastName,
+          Hometown,
+          Earnings,
+          Event,
+          Type,
+          Points,
+          SeasonYear,
+          StandingId,
+        });
+      }
+    });
 
-  const data = rawData.slice(1, 51).filter((position) => position.Place !== 0);
+    const data = rawData
+      .slice(1, 51)
+      .filter((position) => position.Place !== 0);
 
-  const error = null;
+    const error = null;
 
-  return { error, data };
+    return { error, data };
+  } else {
+    const error = "bad url";
+
+    return { error, data: [] };
+  }
 }
