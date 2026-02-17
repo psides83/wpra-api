d617e844d4d7ce0f7201d2f1e31e6c6330aea727
Payton <psides83@hotmail.com>
2026-02-17 08:14:34 -0600

    scraper update

--- functions/wpra_scraper.cjs
+++ functions/wpra_scraper.cjs
@@ -24,7 +24,8 @@ const CIRCUITS = {
   // BRAZIL: { title: "Brazil", id: 15 },
 };
 
-const outputDir = "/Users/Payton/web-development/wpra-json";
+const outputDir =
+  process.env.WPRA_OUTPUT_DIR || "/Users/Payton/web-development/wpra-json";
 
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
 
